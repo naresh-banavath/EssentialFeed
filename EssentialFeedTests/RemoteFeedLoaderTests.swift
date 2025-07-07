d5d9b5e1aac1747679f4ea8a830c521110a39f47
Naresh <banavath.naresh+clh@cleanharbors.com>
2025-07-07 21:03:30 +0530

    replace stubbing bby capturing values instead

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -33,11 +33,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         var capturedErrors =  [RemoteFeedLoader.Error]()
-        
-        client.error = NSError(domain: "Test", code: 0)
-        
         sut.load()  { capturedErrors.append($0) }
- 
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     
@@ -54,16 +53,16 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         
-        var requestedURL: URL?
         var requestedURLs = [URL]()
         var error: Error?
+        var completions = [(Error) -> Void]()
         func get(from url: URL, completion: @escaping (Error?) -> Void) {
-            requestedURL = url
-            requestedURLs.append(url)
+          
             if let error = error {
                 completion(error)
             }
-            
+            completions.append(completion)
+            requestedURLs.append(url)
         }
             
     }
