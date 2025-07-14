c550bd7b002824e7caac098a887b1b70e8ee971d
Naresh <banavath.naresh+clh@cleanharbors.com>
2025-07-14 11:30:54 +0530

    Remove Generic from LoadFeedResult to simplify type contraints

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -16,7 +16,7 @@ public final class RemoteFeedLoader: FeedLoader {
         case connectivity
         case invalidData
     }
-    public typealias Result = LoadFeedResult<Error>
+    public typealias Result = LoadFeedResult
 
     public init(url: URL,client: HTTPClient) {
         self.client = client
@@ -32,7 +32,7 @@ public final class RemoteFeedLoader: FeedLoader {
             case let .success(data, response):
                 completion(FeedItemsWrapper.map(data, response: response))
             case .failure:
-                completion(.failure(.connectivity))
+                completion(.failure(Error.connectivity))
             }
             
         }
