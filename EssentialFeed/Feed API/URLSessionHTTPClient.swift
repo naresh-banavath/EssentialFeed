8f60c8c7567b3846afb19157ce08d83b654eb7fc
Naresh <banavath.naresh+clh@cleanharbors.com>
2025-07-21 22:26:53 +0530

    Move URLSessionHTTPClient to its own production file

---
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,27 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Banavath, Naresh on 21/07/25.
+//
+
+import Foundation
+public class URLSessionHTTPClient: HTTPClient {
+    
+    let session: URLSession
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    struct UnexpectedValuesRepresentation: Error {}
+    public  func get(from url: URL, completion: @escaping (HTTPClientResult) -> ()) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            }else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            }else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
