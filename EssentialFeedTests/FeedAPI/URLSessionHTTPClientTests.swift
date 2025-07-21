cba632fe608ae64b4bbf01612024b5a17bfe6e86
Naresh <banavath.naresh+clh@cleanharbors.com>
2025-07-21 11:47:51 +0530

    move url creation to a factory method

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -37,9 +37,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performsGETRequestsWithURL() {
         
-        let url = URL(string: "https://example.com")!
+      
         let expectation = expectation(description: "wait for request")
-        
+        let url = anyURL()
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
@@ -51,14 +51,14 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     func test_getFromURL_failsOnRequestError() {
         
-        let url = URL(string: "https://example.com")!
+      
         
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil,  error: error)
         
        
         let expectation = expectation(description: "wait for completion")
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL()) { result in
             
             switch result {
             case let .failure(receivedError as NSError):
@@ -79,6 +79,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    private func anyURL() -> URL {
+        return URL(string: "https://example.com")!
     }
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
