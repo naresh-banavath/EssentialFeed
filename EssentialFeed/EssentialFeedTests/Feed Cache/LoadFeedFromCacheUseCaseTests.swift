e46bd49b78ef91343f65b779302bdfdf46780ae1
Naresh Banavath <nareshnayak839@gmail.com>
2026-01-06 11:00:24 +0530

    Load command requests cache retrieval

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -14,6 +14,13 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         let (_, store) = makeSUT()
         XCTAssertEqual(store.receivedMessages, [])
     }
+    func test_load_requestsCacheRetrieval(){
+        let (sut, store) = makeSUT()
+        
+        sut.load()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
     
     
     
